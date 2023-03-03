11875a07824ab6cfa61064bb6c8acd5bcb929c9a
Shantanu Methikar <smethikar18@gmail.com>
2023-03-03 16:54:31 +0530

    Add project files.

---
+++ InteropBetweenJavaScriptAndMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InteropBetweenJavaScriptAndMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ InteropBetweenJavaScriptAndMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InteropBetweenJavaScriptAndMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
